b0b857f1b53aebb3c1c095564b551241a5b9a47e
Yohannes Sibhat <yohannessibhat4@gmail.com>
2023-01-27 23:03:55 -0800

    first commit

---
+++ Car.js/Car.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+const Car = () => {
+    return (
+        <div>
+            <h1>Car Rental system</h1>
+        </div>
+    );
+}
+
+export default Car;
